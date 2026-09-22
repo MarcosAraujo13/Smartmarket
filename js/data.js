3df8eaba345644eb9a8064dad9e813a24ad82ff7
Marcos Galvão De Araujo <marcosck02@gmail.com>
2026-09-22 18:37:58 -0300

    Mecanismo de pesquisa

--- js/data.js
+++ js/data.js
@@ -1,3 +1,8 @@
 const produtos = [
-  { id: 1, nome: "Arroz Branco Camil 1kg", categoria: "Alimento", valor: 5.49, imagem: "image/alimento-1.webp" }
+  {
+    id: 1, 
+    nome: " Arroz Branco Camil 1kg", 
+    valor: 5.49,
+    seção: 15, 
+    imagem: "image/alimento-1.webp" }
 ];
--- js/script.js
+++ js/script.js
@@ -1,5 +1,36 @@
 console.log("js funcionando");
 
+const campoPesquisa = document.getElementById("pesquisa");
+const resultados = document.getElementById("resultados");
+
+campoPesquisa.addEventListener("input", pesquisar);
+
+function pesquisar() {
+
+const textoPesquisado = campoPesquisa.value.toLowerCase();
+
+resultados.innerHTML = "";
+
 produtos.forEach((produto) => {
 
+    if (produto.nome.toLowerCase().includes(textoPesquisado)) {
+
+        resultados.innerHTML += `
+        
+            <div class="produto">
+
+                <span>${produto.nome}</span>
+
+                <span>| Seção ${produto.seção}</span>
+
+                <span> | R$ ${produto.valor.toFixed(2)}</span>
+
+            </div>
+        `;
+
+    }
+
 });
+
+}
+
